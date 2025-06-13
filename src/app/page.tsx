9a3fee955d7266335d71baeb2878c981ff4b53c4
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-14 01:37:44 +0700

    feat: Enhance Home page layout with additional information and styling for image upload limits and supported formats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { CardUploadImageWrapper } from "@/components/uploads/CardUploadImageWrapper";
+import { PiCheck } from "react-icons/pi";
 
 export default function Home() {
   return (
@@ -9,14 +10,32 @@ export default function Home() {
         <div className="absolute bottom-0 right-0 size-96 -z-10 bg-pink-300/20 blur-[100px] rounded-full" />
       </div>
 
-      <div className="w-full min-h-screen flex flex-col items-center justify-center gap-10 bg-background/30 backdrop-blur-sm z-10 px-3">
-        <h1 className="heading ">Convert Image</h1>
-        <p className="sub-heading max-w-2xl text-center">
-          Convert your images to any format for free. Just upload your image below to get started – No registration
-          required!
-        </p>
-        <div className="mx-auto w-full">
+      <div className="w-full min-h-screen flex flex-col items-center justify-center bg-background/30 backdrop-blur-sm z-10 px-3 pt-20 lg:pt-0">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="heading">Convert Image</h1>
+          <p className="sub-heading max-w-2xl text-center">
+            Convert your images to any format for free. Just upload your image below to get started – No registration
+            required!
+          </p>
+        </div>
+        <div className="mx-auto w-full py-5">
           <CardUploadImageWrapper />
+        </div>
+        <div className="w-full flex justify-center py-5">
+          <div className="flex gap-4 text-sm text-gray-600 flex-col md:flex-row">
+            <div className="flex items-center gap-2  border border-accent/20 backdrop-blur-sm rounded-lg p-3">
+              <PiCheck className="text-green-500" />
+              <span className="sub-heading">Maximum 10 images per upload</span>
+            </div>
+            <div className="flex items-center gap-2 border border-accent/20 backdrop-blur-sm rounded-lg p-3">
+              <PiCheck className="text-green-500" />
+              <span className="sub-heading">Supported formats: JPG, PNG, WEBP</span>
+            </div>
+            <div className="flex items-center gap-2 border border-accent/20 backdrop-blur-sm rounded-lg p-3">
+              <PiCheck className="text-green-500" />
+              <span className="sub-heading">Maximum file size: 10MB</span>
+            </div>
+          </div>
         </div>
       </div>
     </section>
