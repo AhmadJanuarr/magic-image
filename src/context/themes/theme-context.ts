75d457426024da0e50c4e6212a1e3fb53a5ee681
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-27 22:26:32 +0700

    feat: Create ThemeContext for managing light and dark themes in the application

---
+++ src/context/themes/theme-context.ts
@@ -0,0 +1,11 @@
+"use client";
+import { createContext } from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
